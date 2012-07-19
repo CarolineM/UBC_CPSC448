ae3570e11d38fe2c5f2c1eddcbb1b7f8f247bca5
Nicholas Chow <nickchow.stretch@gmail.com>
2012-07-18 17:57:52 -0700

    Naive implementation of how to select pixels that fall within a given colour palette.

--- HTML5_ImageManipulation/ImagePixelManipulation.js
+++ HTML5_ImageManipulation/ImagePixelManipulation.js
@@ -2,4 +2,43 @@ function draw(image, context) {
 	context.drawImage(image, 0, 0);
 	context.rotate(Math.PI / 21);
 }
+		
+		
+function onMouseClick(e) {
+		
+}
 
+/*
+ * Given the RGB values of a single pixel, this function returns 1 if our 
+ * pixel falls within the hard-coded colour range.
+ */
+ var redUpperLimit = 255;
+ var redLowerLimit = 235;
+ var greenUpperLimit = 230;
+ var greenLowerLimit = 175;
+ var blueUpperLimit = 108;
+ var blueLowerLimit = 65;
+function inColourRange(red, green, blue)
+{
+	if ( between(redUpperLimit, redLowerLimit, red) )
+	{
+		if ( between(greenUpperLimit, greenLowerLimit, green) )
+		{
+			if (between(blueUpperLimit, blueLowerLimit, blue) )
+			{
+				return 1;
+			}
+			return 0;
+		}
+		return 0;
+	}
+	return 0;
+}
+
+/*
+ * Checks to see if value falls between first and last.
+ */
+function between(first, last, value)
+{
+	return (first < last ? value >= first && value <= last : value >= last && value <= first);
+}

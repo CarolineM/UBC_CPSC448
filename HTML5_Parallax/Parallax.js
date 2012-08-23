6b693f8343a28aada48d1aef8185a10e09b26a9b
Nicholas Chow <nickchow.stretch@gmail.com>
2012-08-23 15:44:18 -0700

    Boilerplate code for layered tweening with easing
    - Added art assets
    - Tweening not yet functional
    - ParallaxOffset will be used to calculate the targetXY for each individual layer
    - The layer-specific targetXYs will then be fed into our BackgroundEasing_RefactoredEasing module.

---
+++ HTML5_Parallax/Parallax.js
@@ -0,0 +1,14 @@
+function ParallaxOffset(focalDepth, layer0Depth, layer1Depth, layer2Depth)
+{
+	var this.focalDepth = focalDepth,
+		this.layer0Depth = layer0Depth,
+		this.layer1Depth = layer1Depth,
+		this.layer2Depth = layer2Depth;
+
+	function calculate_PerLayerTargetXY(focalTargetX, focalTargetY)
+	{
+		
+	}
+}
+
+

6ca18bf9214f05c6f7c3645be462de0aea4c62e2
Nicholas Chow <nickchow.stretch@gmail.com>
2012-09-05 17:37:36 -0700

    Parallax finished

--- HTML5_Parallax/Parallax.js
+++ HTML5_Parallax/Parallax.js
@@ -13,32 +13,39 @@ function parallaxOffset(focalDepth, layer0Depth, layer1Depth, layer2Depth)
 		var	layer1Y;
 		var	layer2X;
 		var	layer2Y;
-		var	angleX = Math.atan(dX0/this.focalDepth);
-		var	angleY = Math.atan(dY0/this.focalDepth);
-		var	dX0 = focalTargetX - sceneryView0.x;
-		var	dY0 = focalTargetY - sceneryView0.y;
-		var	dX1 = Math.tan(angleX)*Math.abs(this.focalDepth - this.layer1Depth);
-		var	dY1 = Math.tan(angleY)*Math.abs(this.focalDepth - this.layer1Depth);
-		var	dX2 = Math.tan(angleX)*Math.abs(this.focalDepth - this.layer2Depth);
-		var	dY2 = Math.tan(angleY)*Math.abs(this.focalDepth - this.layer2Depth);
+		var	dX2 = focalTargetX - sceneryView2.x;
+		var	dY2 = focalTargetY - sceneryView2.y;
+		//var	angleX = Math.atan(dX0/this.focalDepth);
+		//var	angleY = Math.atan(dY0/this.focalDepth);
+		var	dX1 = (dX2/this.focalDepth)*Math.abs(this.focalDepth - this.layer1Depth);
+		var	dY1 = (dY2/this.focalDepth)*Math.abs(this.focalDepth - this.layer1Depth);
+		var	dX0 = (dX2/this.focalDepth)*Math.abs(this.focalDepth - this.layer0Depth);
+		var	dY0 = (dY2/this.focalDepth)*Math.abs(this.focalDepth - this.layer0Depth);
 
-		layer0X = sceneryView0.x - dX0;
-		layer0Y = sceneryView0.y + dY0;
+		layer2X = sceneryView2.x + dX2;
+		layer2Y = sceneryView2.y + dY2;
 		
-		if (this.focalDepth < this.layer1Depth)
+		if (this.focalDepth <= this.layer1Depth)
 		{
-			layer1X = sceneryView1.x + dX1;
+			layer1X = sceneryView1.x - dX1;
 			layer1Y = sceneryView1.y - dY1;
-			layer2X = sceneryView2.x - dX2;
-			layer2Y = sceneryView2.y + dY2;
+			layer0X = sceneryView0.x - dX0;
+			layer0Y = sceneryView0.y - dY0;
 		}
 		if (this.focalDepth > this.layer1Depth)
 		{
-			layer1X = sceneryView1.x - dX1;
+			layer1X = sceneryView1.x + dX1;
 			layer1Y = sceneryView1.y + dY1;
-			layer2X = sceneryView2.x + dX2;
-			layer2Y = sceneryView2.y - dY2;
+			layer0X = sceneryView0.x - dX0;
+			layer0Y = sceneryView0.y - dY0;
+			
+			if (this.focalDepth > this.layer0Depth)
+			{
+				layer0X = sceneryView0.x + dX0;
+				layer0Y = sceneryView0.y + dY0;
+			}
 		}
+
 
 		return {layer0X: layer0X, layer0Y: layer0Y, 
 				layer1X: layer1X, layer1Y: layer1Y, 
